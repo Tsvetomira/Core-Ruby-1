e0fc60f2b83d7ae89ad061643e86fc8732beec55
Tsvetomira <ccnikolova@gmail.com>
2014-10-28 19:46:07 +0200

    Update solution.rb

--- week1/2-hashes/solution.rb
+++ week1/2-hashes/solution.rb
@@ -1,3 +1,42 @@
 class Hash
-  # Your code goes here.
+  def pick(*keys)
+    
+     self.select {|key,value| keys.include?(key)} 
+        
+  end
 end
+
+class Hash
+  def except(*keys)
+    
+     self.delete_if {|key,value| !keys.include?(key)} 
+        
+  end
+end
+
+class Hash
+  def compact_values
+    self.delete_if {|key,value| value==nil || value==true || value==false }
+  end
+end
+
+class Hash
+  def defaults(hash)
+    hash.merge(self) 
+  end
+end
+
+class Hash
+  def defaults!(hash)
+    hash.merge!(self) 
+  end
+end
+
+class Hash
+  def pick!(*keys)
+    
+     self.select! {|key,value| keys.include?(key)} 
+        
+  end
+end
+
